2e494413f41292504cb63a98baea189028b57dee
Ian Harris <iankerriganharris@gmail.com>
2018-08-19 10:32:22 -0700

    completed user live search

--- client/src/components/GeneralSearch.js
+++ client/src/components/GeneralSearch.js
@@ -1,64 +1,74 @@
 import React from 'react';
-import { Input, Dropdown, Menu } from 'antd';
+import { Input, Dropdown, Menu, Spin, Icon } from 'antd';
 import { Link } from 'react-router-dom';
 import axios from "axios";
 
-const Search = Input.Search;
-
 class GeneralSearch extends React.Component {
+  componentWillMount() {
+    this.resetComponent()
+  }
 
   resetComponent = () => {
-    console.log('reset called')
-    this.setState({showResults: false})
+    this.setState({showResults: false, loading: false})
   }
 
   state = {
     showResults: false,
     value: [],
-    data: []
-  }
-
-  doSearch = async (terms) => {
-    const request = {
-      credentials: 'include',
-      method: 'GET'
-    }
-    const response = await axios(`/api/search?q=${terms}`, request)
-    if (response.status >= 200 && response.status < 300) {
-      const data = response.data.map(hit => ({
-        text: hit._source.screen_name,
-        value: hit._source.screen_name
-      }))
-      this.setState({ data: [...data], showResults: true})
-    } else {
-      const error = response.statusText
-    }
+    data: [],
+    loading: false
   }
 
   handleChange = (value) => {
-    this.setState({
-      value,
-      data: [],
-      fetching: false,
-    });
+    console.log(value)
+    this.setState({value, data: [], loading: true},
+      () => {
+        setTimeout(async () => {
+          if (this.state.value.length < 1) return this.resetComponent()
+          const request = {
+            credentials: 'include',
+            method: 'GET'
+          }
+          const response = await axios(`/api/search?q=${value}`, request)
+          if (response.status >= 200 && response.status < 300) {
+            const data = response.data.map(hit => ({
+              text: hit._source.screen_name,
+              value: hit._source.screen_name
+            }))
+            this.setState({ data: [...data], showResults: true, loading: false})
+          } else {
+            const error = response.statusText
+          }
+        }, 300);
+      }
+    )
   }
 
   render() {
-    const { value, data, showResults } = this.state
+    const { value, data, showResults, loading } = this.state
     const dataList = data.length ? data.map(
       hit =>
         <Menu.Item key={hit.value}>
           <Link onClick={() => this.resetComponent()} to={`/${hit.value}`}>{hit.value}</Link>
         </Menu.Item>
-    ) : <Menu.Item></Menu.Item>
+    ) : <Menu.Item>No results found.</Menu.Item>
     const results = <Menu>{dataList}</Menu>
+    console.log('show results ' + showResults)
     return(
-      <div>
-        <Search
-          placeholder="search..."
-          onSearch={this.doSearch}
-        />
-        <Dropdown visible={this.state.showResults} overlay={results}><div></div></Dropdown>
+      <div className={this.props.className}>
+        <Spin size={"small"} spinning={loading}>
+          <Input
+            prefix={<Icon type="search"/>}
+            placeholder="search..."
+            onChange={(e) => this.handleChange(e.target.value)}
+          />
+        </Spin>
+        {showResults ?
+          <Dropdown visible overlay={results}>
+            <div></div>
+          </Dropdown>
+          : null
+        }
       </div>
     )
   }
--- client/src/components/PrivateHeader.js
+++ client/src/components/PrivateHeader.js
@@ -1,44 +1,58 @@
 import React from 'react';
 import {NavLink, Route} from 'react-router-dom';
-import { Menu, Icon } from 'antd';
+import { Row, Col, Menu, Icon } from 'antd';
 import GeneralSearch from "./GeneralSearch";
 
 const PrivateHeader = (props) => {
   return (
     <div>
-      <Menu
-        mode='horizontal'
-        selectedKeys={[props.location.pathname]}
-      >
-        <Menu.Item key='/'>
-          <NavLink to='/' activeClassName='selected'>
-            <Icon type="home" />Home
-          </NavLink>
-        </Menu.Item>
-        <Menu.Item key='/discover'>
-          <NavLink to='/discover' activeClassName='selected'>
-            <Icon type="share-alt" />Discover
-          </NavLink>
-        </Menu.Item>
-        <Menu.Item key='/profile'>
-          <NavLink to='/profile' activeClassName='selected'>
-            <Icon type="idcard" />Profile
-          </NavLink>
-        </Menu.Item>
-        <Menu.Item key='/question'>
-          <NavLink to='/question' activeClassName='selected'>
-            <Icon type="question" />Question
-          </NavLink>
-        </Menu.Item>
-        <Menu.Item style={{float:'right'}} key='/logout'>
-          <NavLink to='/logout' activeClassName='selected'>
-            <Icon type="logout" />Logout
-          </NavLink>
-        </Menu.Item>
-        <Menu.Item disabled style={{float:'right', width: '20%'}} >
-          <GeneralSearch />
-        </Menu.Item>
-      </Menu>
+      <Row type='flex' align='middle'>
+        <Col span={18}>
+          <Menu
+            mode='horizontal'
+            selectedKeys={[props.location.pathname]}
+          >
+            <Menu.Item key='/'>
+              <NavLink to='/' activeClassName='selected'>
+                <Icon type="home" />Home
+              </NavLink>
+            </Menu.Item>
+            <Menu.Item key='/discover'>
+              <NavLink to='/discover' activeClassName='selected'>
+                <Icon type="share-alt" />Discover
+              </NavLink>
+            </Menu.Item>
+            <Menu.Item key='/profile'>
+              <NavLink to='/profile' activeClassName='selected'>
+                <Icon type="idcard" />Profile
+              </NavLink>
+            </Menu.Item>
+            <Menu.Item key='/question'>
+              <NavLink to='/question' activeClassName='selected'>
+                <Icon type="question" />Question
+              </NavLink>
+            </Menu.Item>
+          </Menu>
+        </Col>
+        <Col span={4}>
+            <GeneralSearch className='ant-menu-horizontal' />
+        </Col>
+        <Col span={2}>
+          <Menu
+            mode='horizontal'
+            selectedKeys={[props.location.pathname]}
+          >
+          <Menu.Item style={{float:'right'}} key='/logout'>
+            <NavLink to='/logout' activeClassName='selected'>
+              <Icon type="logout" />Logout
+            </NavLink>
+          </Menu.Item>
+          </Menu>
+          {/*<Menu.Item className="no-hover" style={{float:'right', width: '20%'}} >*/}
+
+          {/*</Menu.Item>*/}
+        </Col>
+      </Row>
     </div>
   )
 }
